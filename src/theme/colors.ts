7fac835dd13097101ad2415bd4ddd9b591538af9
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-29 11:13:21 -0300

    Feat: publish

---
+++ src/theme/colors.ts
@@ -0,0 +1,56 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#29aa69",
+  primaryBright: "#29aa69",
+  primaryDark: "#29aa69",
+  secondary: "#7645D9",
+  success: "#31D0AA",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#FAF9FA",
+  backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  tertiary: "#EFF4F5",
+  text: "#452A7A",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#8f80ba",
+  borderColor: "#E9EAEB",
+  card: "#FFFFFF",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#ecd76e",
+  background: "#0c1b38",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#EAE2FC",
+  textDisabled: "#666171",
+  textSubtle: "#b3ca35",
+  borderColor: "#524B63",
+  // card: "#27262c",
+  card: "#071329",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+  },
+};
